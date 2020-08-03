c76180ad64b66e2c552817c9dcaa08b92ac09836
shi-vani233 <68747043+shi-vani233@users.noreply.github.com>
2020-08-03 19:55:37 +0530

    Add files via upload

---
+++ LAB_4/Image.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace Lab4_task2
+{
+    public partial class Image : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            image1.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(Button1.Text=="hide")
+            {
+                Button1.Text = "show";
+
+                image1.Visible = false;
+            }
+            else
+            {
+                Button1.Text = "hide";
+
+                image1.Visible = true;
+            }
+        }
+    }
+}
