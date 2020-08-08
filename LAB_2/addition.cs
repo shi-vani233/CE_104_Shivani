3d015df721129cb4cdb70fcc3a81a03ca733b0c2
shi-vani233 <68747043+shi-vani233@users.noreply.github.com>
2020-08-08 21:00:22 +0530

    Add files via upload

---
+++ LAB_2/addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task
+{
+    public class class1
+    {
+        public int addition(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ LAB_2/multiplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task
+{
+    public class class2
+    {
+        public int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
