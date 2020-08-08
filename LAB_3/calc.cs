eb98151f992576c859fd76f111fdc139b0a22474
shi-vani233 <68747043+shi-vani233@users.noreply.github.com>
2020-08-08 21:21:49 +0530

    Add files via upload

---
+++ LAB_3/calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3
+{
+    public class calc
+    {
+        public int addition(int x, int y)
+        {
+            return x + y;
+        }
+        public int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ LAB_3/calc_new.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+[assembly: AssemblyVersion("1.0.0.0")]
+
+
+namespace lab_3
+{
+    public class calc
+    {
+        public int addition(int x, int y)
+        {
+            Console.WriteLine("new version");
+            return x + y;
+        }
+        public int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
