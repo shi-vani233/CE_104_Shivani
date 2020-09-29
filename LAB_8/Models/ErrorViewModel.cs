03ca5bc47a97fc306cc5edb4d8dac39f1b68498b
shi-vani233 <68747043+shi-vani233@users.noreply.github.com>
2020-09-29 11:02:26 +0000

    Add files via upload

---
+++ LAB_8/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lab8_task3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
